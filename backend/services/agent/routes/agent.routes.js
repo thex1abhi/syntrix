85e7e80f7949c906fa8d903d152851f8c1db85e5
thex1abhi <yabhishekk480@gmail.com>
2026-08-28 22:46:27 +0530

    Agent chat  route

---
+++ backend/services/agent/routes/agent.routes.js
@@ -0,0 +1,12 @@
+
+
+
+import express from "express"
+import { agent } from "../controllers/agent.controller.js";
+
+const router = express.Router();
+
+router.post("/chat", agent)
+
+
+export default router  
