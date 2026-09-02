28a26a8799b5569820b2f595a6d2721d0ba6cfbf
thex1abhi <yabhishekk480@gmail.com>
2026-09-02 23:03:37 +0530

    Memory and  chat history setup

--- backend/services/agent/agents/chat.agent.js
+++ backend/services/agent/agents/chat.agent.js
@@ -1,18 +1,47 @@
+import { AIMessage, HumanMessage, SystemMessage } from "@langchain/core/messages"
 import { getModel } from "../config/llmmodel.js"
+import { getMemory } from "../config/memory.js"
 
 export const chatAgent = async (state) => {
 
+
     const llm = await getModel("chat")
-    const systemPrompt = "You are syntrix AI , an intelligent AI assistance"
-    const response = await llm.invoke([
-        {
-            "role": "system",
-            "content": systemPrompt,
-        }, {
-            "role": "human",
-            "content": state.prompt
+    const history = await getMemory(state.conversationId)
+
+    const systemPrompt = `You are syntrix AI , an intelligent AI assistance.
+    Rules: 
+    -For simple questions , greetings, and short queries , respond naturally in plain text.
+    -For technical , educational , coding  , or detailed topics , use clean Markdown .
+
+    Formatting : 
+   - Use # for title and ## for sections.
+    - Leave  a blank line after heading.
+    -Use bullet points for lists.
+    - Use numbered lists for steps. 
+    - Use fenced  code blocks with language tags for code.
+    -Keep paragaphs short and readable.
+    -Never write headings and content on the same line. 
+    - Never generate large walls of text. 
+     `
+
+    const messages = [
+        new SystemMessage(systemPrompt)
+    ]
+
+    history.forEach(msg => {
+        if (msg.role == "user") {
+            messages.push(new HumanMessage(msg.content))
         }
-    ])
+        if (msg.role == "assistant") {
+            messages.push(new AIMessage(msg.content))
+        }
+    });
+
+    messages.push(new HumanMessage(state.prompt))
+    console.log(messages);
+
+
+    const response = await llm.invoke(messages)
     return {
         ...state,
         aiResponse: response.content
--- backend/services/agent/controllers/agent.controller.js
+++ backend/services/agent/controllers/agent.controller.js
@@ -1,10 +1,13 @@
 
 import axios from "axios"
 import { graph } from "../graph/graph.js"
+import { addMessage } from "../config/memory.js"
 
 export const agent = async (req, res) => {
     try {
         const { prompt, conversationId } = req.body
+
+        await addMessage(conversationId, "user", prompt)
         await axios.post(`${process.env.CHAT_SERVICE}/save-message`, {
             conversationId, role: "user", content: prompt
         })
@@ -15,11 +18,12 @@ export const agent = async (req, res) => {
         })
 
         const response = result.aiResponse
+        await addMessage(conversationId, "assistant", response)
         await axios.post(`${process.env.CHAT_SERVICE}/save-message`, {
             conversationId, role: "assistant", content: response
         })
         return res.status(200).json(response)
-        
+
     } catch (error) {
         return res.status(500).json({ message: `agent error ${error}` })
     }
