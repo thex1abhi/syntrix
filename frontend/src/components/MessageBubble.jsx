f51517aa35946095a095f32d42ac271b50c372b3
thex1abhi <yabhishekk480@gmail.com>
2026-09-04 23:43:55 +0530

    Markdown component

--- frontend/src/components/MessageBubble.jsx
+++ frontend/src/components/MessageBubble.jsx
@@ -1,10 +1,13 @@
 
-import React from "react";
+import React, { useState } from "react";
 import MarkDown from "react-markdown"
+import { X } from "lucide-react"
+import remarkGfm from 'remark-gfm'
 
 function MessageBubble({ role, content, images }) {
 
     const isUser = role === "user"
+    const [lightBox, setLightBox] = useState(null);
 
     return (
         <>
@@ -17,16 +20,55 @@ function MessageBubble({ role, content, images }) {
                         <div className="flex flex-wrap gap-3 mt-4 ">
                             {images.map((img, i) => (
                                 <img key={i}
-                                    loading="lazy" 
-                                    src={img} alt="" className="" />
+                                    loading="lazy"
+                                    onClick={() => setLightBox(img)}
+                                    onError={(e) => e.currentTarget.remove()}
+                                    src={img} alt="" className="w-40 h-28 rounded-xl object-cover border border-white/10 cursor-zoom-in hover:opacity-90 transition " />
                             ))}
                         </div>
                     )}
 
-                    <MarkDown>
+                    <MarkDown remarkPlugins={[remarkGfm]}
+                        components={{
+                            h1: ({ children }) => (
+                                <h1 className="text-2xl font-bold mt-5 mb-3  " > {children} </h1>
+                            ),
+                            h2: ({ children }) => (
+                                <h1 className="text-xl font-seminbold  mt-4 mb-2  " > {children} </h1>
+                            ),
+                            h3: ({ children }) => (
+                                <h1 className="text-lg  font-semibold  mt-3 mb-2   " > {children} </h1>
+                            ),
+                            p: ({ children }) => (
+                                <p className=" mb-3 whitespace-pre-wrap  break-words   " > {children} </p>
+                            ),
+                            ul: ({ children }) => (
+                                <ul className=" list-disc pl-5 space-y-1  my-2   " > {children} </ul>
+                            ),
+
+
+                        }}
+                    >
                         {content}
                     </MarkDown>
+
                 </div>
+
+
+                {/* imgs  */}
+                {lightBox &&
+                    <div className="fixed inset-0 z-50  bg-black/80 backdrop-blur-sm flex items-center justify-center p-6  ">
+                        <button
+                            onClick={() => setLightBox(null)}
+                            className=" absolute top-5 right-5 text-white/80 hover:text-white 
+                            bg-white/10 rounded-full p-2  cursor-pointer " >
+                            <X />
+                        </button>
+                        <img src={lightBox} alt="imgError" className=" max-w-[90vw] max-h-[85vh] 
+                        rounded-2xl  border border-white/10 shadow-2xl object-contain " />
+                    </div>
+                }
+
             </div>
         </>
     )
