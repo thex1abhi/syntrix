6aa37241d38e93b756e66b871a9a2e0527daf63f
thex1abhi <yabhishekk480@gmail.com>
2026-09-13 19:31:52 +0530

    Implementing ppt agent

--- backend/services/agent/agents/ppt.agent.js
+++ backend/services/agent/agents/ppt.agent.js
@@ -1,6 +1,54 @@
+import { getModel } from "../config/llmModel.js";
 
 
 
-export const pptAgent=async () => {
-    
+export const pptAgent = async (state) => {
+    try {
+        const llm = await getModel("ppt")
+        const prompt = `   
+        You are a professional presentation designer .
+
+        Return ONLY valid JSON .
+
+        Format : 
+          {
+                "title":"",
+                "subtitle":"",
+                "slides":[
+                { 
+                "title":"",
+                "points":[
+                "",
+                "",
+                "",
+                "",
+                ]
+                }
+                ]
+                } 
+                
+                Rules : 
+                -Generate exactly 6 concise slides.
+                -Each slide should have 4-6 concise bullet points.
+                -No markdown.
+                -No explanation.
+                -No code block.
+                -Return ONLY JSON.
+
+     Topic :
+        ${state.prompt}
+`
+        const res = await llm.invoke(prompt)
+        console.log(JSON.parse(res.content));
+        
+
+    } catch (error) {
+        console.log(error);
+        return {
+            ...state,
+            aiResponse: `
+           ❌ Failed to generate ppt 
+            `
+        }
+    }
 }
