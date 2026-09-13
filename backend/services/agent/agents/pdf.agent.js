2ae5dff9f83d21dc7bc2b1b01188b582b3529f58
thex1abhi <yabhishekk480@gmail.com>
2026-09-13 13:06:27 +0530

    Implemented Pdf agent

--- backend/services/agent/agents/pdf.agent.js
+++ backend/services/agent/agents/pdf.agent.js
@@ -1,6 +1,64 @@
+import { getModel } from "../config/llmModel.js";
+import { generatePdf } from "../utils/generatePdf.js";
+import { getFromS3 } from "../utils/getFromS3.js";
+import { uploadToS3 } from "../utils/uploadToS3.js";
 
+export const pdfAgent = async (state) => {
+    try {
+        const llm = await getModel("pdf")
+        const prompt = ` 
+        You are an expert document writer.
 
+        Return ONLY valid JSON .
+        Do NOT return markdown .
+        Do NOT return explanations .
 
-export const pdfAgent=async () => {
-    
+        Structure :
+        {
+        "title":"",
+        "subtitle":"",
+        "sections":[
+        { 
+        "heading":"",
+        "points":[]
+        }
+        ]
+        } 
+
+        Generate 4-8 sections.
+
+        Each section should have 3-6 concise bullet points.
+
+        Topic: 
+        ${state.prompt}
+        
+        `
+
+        const res = await llm.invoke(prompt)
+        const data = JSON.parse(res.content)
+        const pdfBuffer = await generatePdf(data)
+
+        const filename = `pdf-${Date.now()}.pdf`
+        await uploadToS3(filename, pdfBuffer, "application/pdf")
+
+        const downloadUrl = await getFromS3(filename, 24 * 60)
+
+        return {
+            ...state,
+            aiResponse: `✔ Pdf Generated 
+
+**${data.title}**
+
+⤵ [Download pdf](${downloadUrl})
+
+⏳ Link expires in 10 minutes.`
+        }
+
+    } catch (error) {
+        console.log(error);
+        return {
+            ...state,
+            aiResponse: "  Failed to generate pdf "
+        }
+    }
 }
--- backend/services/agent/agents/vision.agent.js
+++ backend/services/agent/agents/vision.agent.js
@@ -40,16 +40,16 @@ export const visionAgent = async (state) => {
         const filename = `image-${Date.now()}.png`
 
         await uploadToS3(filename, buffer, "image/png")
-        const downloadUrl = await getFromS3(filename, 24 * 60 * 60,)
+        const downloadUrl = await getFromS3(filename, 24 * 60)
         return {
             ...state,
             aiResponse: `# ✔ Image Generated Successfully
 
 ![Generated Image](${downloadUrl})
 
-[Download Image](${downloadUrl})
+⤵ [Download Image](${downloadUrl})
 
-Link expires in 10 minutes.`
+⏳ Link expires in 10 minutes.`
         };
     } catch (error) {
         console.log(error);
